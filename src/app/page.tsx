284468295f09ee1b9dd1fc83ddfaa6e1fe25ba08
Chirag Singh <chiragdhouni20@gmail.com>
2025-01-02 21:35:27 +0530

    db connectivity fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import logo from "@/assets/logo.png";
 import resumePreview from "@/assets/resume-preview.jpg";
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/prisma";
 import Image from "next/image";
 import Link from "next/link";
 
